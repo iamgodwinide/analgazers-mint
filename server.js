877a557272bb8d111ffe3359a49652bf933577f7
Godwin Idemudia <godwinide@gmail.com>
2023-04-08 17:28:07 +0100

    abi change

--- server.js
+++ server.js
@@ -11,3 +11,15 @@ app.use("*", (req, res) => {
 const PORT = 9045;
 
 app.listen(PORT, () => console.log(`server start on port ${PORT}`));
+
+server{
+    server_name test.analgazers.wtf www.www.test.analgazers.wtf;
+    location / {
+        proxy_pass http://localhost:9045; #whatever port your app runs on
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host $host;
+        proxy_cache_bypass $http_upgrade;
+    }
+}
